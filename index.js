958ccbba7d0c09b666e31201f65241837dd31f38
HANS <151702640+HanzxRYU@users.noreply.github.com>
2024-05-07 13:29:58 +0700

    Tugas DI tanggal 07/05/2024
    Pembahasan tentang perhitungan toko dan system kerja dari matematika dalam data

---
+++ index.js
@@ -0,0 +1,19 @@
+var nama_toko = "Toko BUKU ANDI";
+
+var buku_1 = "Buku Tailwind";
+
+var harga_1 = 12000;
+
+var jumlahbeli = 6;
+
+var total = harga_1 * jumlahbeli;
+
+console.log(nama_toko);
+
+console.log(buku_1);
+
+console.log(harga_1);
+
+console.log(jumlahbeli);
+
+console.log(total);
